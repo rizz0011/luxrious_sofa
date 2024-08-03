5b2b1901884919c4ad666983ca15e4bf61b88bc1
Rizwan Ahmad Khan <ahmadkhanrizwan0@gmail.com>
2024-08-03 16:48:15 +0530

    fix home page issue page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import image from "../../../src/Assest/homeBac.jpg";
-import {  Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 
-const root = {
-  backgroundImage: `url(${image})`,
+const placeholderStyle = {
   height: "95vh",
   backgroundSize: "cover",
   backgroundPosition: "top 70% right 0",
   padding: "0px 30px",
+  backgroundColor: "#f0f0f0", // Placeholder background color
 };
 
 function Home() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = image;
+    img.onload = () => setImageLoaded(true);
+  }, []);
+
+  const root = {
+    ...placeholderStyle,
+    backgroundImage: imageLoaded ? `url(${image})` : 'none',
+  };
+
   return (
     <div className="about-us" style={root}>
-    <Grid container spacing={1}>
-        <Grid
-        className="brand_name"
-          item
-          sm={12}
-          xs={12}
-        >
-          <span >Luxurious Sofa Furniture</span>
+      <Grid container spacing={1}>
+        <Grid className="brand_name" item sm={12} xs={12}>
+          <span>Luxurious Sofa Furniture</span>
         </Grid>
         <Grid item sm={12} xs={12} className="homeService">
-        <span>Morden Sofa, Head Boards Manufactured and Repair Service</span>
+          <span>Modern Sofa, Head Boards Manufactured and Repair Service</span>
         </Grid>
         <Grid item sm={12} xs={12} className="homeAbout">
-          <span> Over 1000 customers have chosen our luxurious sofa furniture and headboards for manufacturing and repairs since 2020.</span>
+          <span>
+            Over 1000 customers have chosen our luxurious sofa furniture and headboards for manufacturing and repairs since 2020.
+          </span>
         </Grid>
-        {/* <Grid item sm={12} xs={12} className="buttonLmore">
-          <button>Learn More</button>
-        </Grid> */}
       </Grid>
     </div>
   );
